b2edb314b017c37715d454e2d1ae73bc9d0b8f91
pranjal <pranjals94@gmail.com>
2023-04-06 18:46:58 +0530

    Chandan prabhu cultivator codes updated

--- Modules/cultivator.py
+++ Modules/cultivator.py
@@ -1,3 +1,28 @@
+# import sys
+#
+# sys.path.append("..")
+#
+# from fastapi import Depends, APIRouter, Request, Response, HTTPException
+# from Modules import auth
+# from sqlalchemy.orm import Session
+# from models import model
+# from schemas import schema
+#
+# router = APIRouter(
+#     prefix="/cultivator",
+#     tags=["cultivator"],
+#     responses={401: {"user": "Not Authorised"}},
+# )
+#
+# @router.get("/assigned_persons/{cultivator_id}")
+# async def get_guests(cultivator_id: int, db: Session = Depends(auth.get_db)):
+#     user = db.query(model.User).filter(model.User.role_id == 2).filter(model.User.person_id == cultivator_id).first()
+#     if user == None:
+#         return {"msg": "Cultivator not found"}
+#     assigned_persons = db.query(model.Person).filter(model.Person.cultivator_id == cultivator_id).offset(0).limit(
+#         50).all()
+#     return {"assigned_persons": assigned_persons}
+
 import sys
 
 sys.path.append("..")
@@ -19,6 +44,29 @@ async def get_guests(cultivator_id: int, db: Session = Depends(auth.get_db)):
     user = db.query(model.User).filter(model.User.role_id == 2).filter(model.User.person_id == cultivator_id).first()
     if user == None:
         return {"msg": "Cultivator not found"}
-    assigned_persons = db.query(model.Person).filter(model.Person.cultivator_id == cultivator_id).offset(0).limit(
-        50).all()
+    # assigned_persons = db.query(model.Person).filter(model.Person.cultivator_id == cultivator_id).offset(0).limit(
+    #     50).all()
+
+    # below list for currently checked in visitors assigned to a cultivator
+    assigned_persons = db.query(model.Person).filter(model.Visit.person_id==model.Person.id).all()
     return {"assigned_persons": assigned_persons}
+
+    #below list for all visitors assigned to a cultivator
+    # assigned_persons = db.query(model.Person).filter(model.Person.cultivator_id==cultivator_id).all()
+    # return {"assigned_persons": assigned_persons}
+@router.get("/assigned_persons_all/{cultivator_id}")
+async def get_guests(cultivator_id: int, db: Session = Depends(auth.get_db)):
+    user = db.query(model.User).filter(model.User.role_id == 2).filter(model.User.person_id == cultivator_id).first()
+    if user == None:
+        return {"msg": "Cultivator not found"}
+    # assigned_persons = db.query(model.Person).filter(model.Person.cultivator_id == cultivator_id).offset(0).limit(
+    #     50).all()
+
+    # below list for currently checked in visitors assigned to a cultivator
+    # assigned_persons = db.query(model.Person).filter(model.Visit.person_id==model.Person.id).all()
+    # return {"assigned_persons": assigned_persons}
+
+    #below list for all visitors assigned to a cultivator
+    assigned_persons = db.query(model.Person).filter(model.Person.cultivator_id==cultivator_id).all()
+    return {"assigned_persons": assigned_persons}
+
